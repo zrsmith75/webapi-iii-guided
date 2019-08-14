25e5fdcd38b4ee1c4eaea91eb944159b6c99f233
Zac Smith <zrs3141592@gmail.com>
2019-08-14 16:39:31 -0600

    update and install express and helmet

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ In this project we will learn how to create a very simple Web API using `Node.js
 - [ ] type `npm i` to download dependencies.
 
 Please follow along as the instructor creates and adds `middleware` to the API step by step.
+
+[YouTube Tutorial](git@github.com:zrsmith75/webapi-iii-guided.git)
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
+const bodyParser = express.json();
 
-server.use(express.json());
+// Global middleware
 
-server.use('/api/hubs', hubsRouter);
+// built in middleware
+server.use(bodyParser);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+// third part middleware
+
+server.use("/api/hubs", hubsRouter);
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
